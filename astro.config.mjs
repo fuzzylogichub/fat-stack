a5edcbef6cff9306fa255e1dd47a4c110647bfd6
Giles Butler <im@giles.io>
2025-03-31 20:05:23 +1100

    update config and packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  experimental: {
-    actions: true,
-  },
   trailingSlash: "never",
   output: "server",
-  adapter: vercel(),
+  adapter: netlify()
 });
