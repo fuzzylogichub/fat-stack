85676fbd78b2d317addda6e242b7a676eeeac274
Giles Butler <im@giles.io>
2025-03-31 20:10:16 +1100

    update firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,17 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCvMoUz0cPIcz_X5KtFB7lvTyO-9HIAk64",
-  authDomain: "astro-d4909.firebaseapp.com",
-  projectId: "astro-d4909",
-  storageBucket: "astro-d4909.appspot.com",
-  messagingSenderId: "552636560911",
-  appId: "1:552636560911:web:273bac353781fecf5a557a",
+  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
